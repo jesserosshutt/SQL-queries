303c3930db809030c8e0770d1bd313d5b4ac9023
Jesse Hutt <jesserosshutt@gmail.com>
2022-01-24 10:52:14 +1300

    add bulk_insert

---
+++ Bulk_Insert_From_CSV.sql
@@ -0,0 +1,12 @@
+BULK INSERT SHAZAM 
+FROM 'C:\Users\JesseHutt\Downloads\shazamlibrary (1).csv' 
+WITH (
+	FORMAT = 'CSV', 
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',', 
+	ROWTERMINATOR = '\n'
+) 
+GO
+
+
+-- Need to create table first including correct columns --
